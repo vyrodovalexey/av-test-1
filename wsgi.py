1804a207b1f4a6096b915777b63510706de8d034
vyrodovalexey <vyrodov.alexey@gmail.com>
2017-05-18 10:49:32 +0300

    2017051802

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-@application.route("/")
-@application.route("/<name>")
+@app.route("/")
+@app.route("/<name>")
 def main(name=" "):
 #    return "Welcome!"
     return render_template('index.html',name=name)
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
